c2aae727c2e3441031bdfbbdb0162557c5970a82
Cristina Torres <ctorresinfo@gmail.com>
2014-07-02 12:50:12 +0200

    Ampliar Ficha - error
    Revisar el SQL que contiene un error que hace que el código  no funcione

--- country.php
+++ country.php
@@ -12,14 +12,29 @@ $connection = mysqli_connect("localhost", "traveller","secret", "world");
 //var_dump($connection); //verificamos que conecta
 
 $country = $_GET["country"];
-$q ="SELECT local_name, head_of_state
-FROM country
-Where code ='$country'";
+$q ="SELECT local_name,
+            head_of_state,
+            continent,
+            region,
+            population,
+            city.name
+    FROM country
+    JOIN city ON city.id = country.capital
+    Where code ='$country'"; //revisar, algun error en el SQL x revisar
 $result = mysqli_query($connection, $q);
 $row = mysqli_fetch_assoc($result);
 
+
 echo "<h1>".$row["local_name"] ."</h1>";
 echo "<h1>".$row["head_of_state"] ."</h1>";
+echo "<h1>".$row["continent"] ."</h1>";
+echo "<h1>".$row["region"] ."</h1>";
+echo "<h1>".$row["population"] ."</h1>";
+echo "<h1>".$row["name"] ."</h1>";
+
+
+
+
 
 ?>
 
