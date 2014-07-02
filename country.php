a0932685f64fd89f9495461f43ed74a383bd6247
Cristina Torres <ctorresinfo@gmail.com>
2014-07-02 11:09:22 +0200

    Mostrar Nombre local Austria

--- country.php
+++ country.php
@@ -2,17 +2,22 @@
 <!Doctype html>
 <html lang="es">
 <head>
-    <meta charset="utf-8">
+    <meta charset="iso-8859-1">
     <title>World App</title>
 </head>
 <body>
 <?php
 $connection = mysqli_connect("localhost", "traveller","secret", "world");
 
-var_dump($connection);
+//var_dump($connection); //verificamos que conecta
 
-$q ="SELECT NOW()";
+$q ="SELECT local_name
+FROM country
+Where code ='AUT'";
 $result = mysqli_query($connection, $q);
+$row = mysqli_fetch_assoc($result);
+
+echo "<h1>".$row["local_name"] ."</h1>";
 ?>
 
 
