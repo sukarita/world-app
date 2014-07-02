837350d4aa60af4140a0e7c055f0905165c42f06
Cristina Torres <ctorresinfo@gmail.com>
2014-07-02 11:13:51 +0200

    Query Nombre local y Cabeza de Estado

--- country.php
+++ country.php
@@ -11,13 +11,15 @@ $connection = mysqli_connect("localhost", "traveller","secret", "world");
 
 //var_dump($connection); //verificamos que conecta
 
-$q ="SELECT local_name
+$q ="SELECT local_name, head_of_state
 FROM country
 Where code ='AUT'";
 $result = mysqli_query($connection, $q);
 $row = mysqli_fetch_assoc($result);
 
 echo "<h1>".$row["local_name"] ."</h1>";
+echo "<h1>".$row["head_of_state"] ."</h1>";
+
 ?>
 
 
